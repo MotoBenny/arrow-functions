723fc58dafa3f5cb325abb7410ab29e438b5808c
Benjamin Carter <benj.james.carter@gmail.com>
2022-03-10 15:35:34 -0800

    completed arrow functions worksheet

--- starter-code/app.js
+++ starter-code/app.js
@@ -160,27 +160,22 @@ let Student = function(name, age, hometown) {
   this.hometown = hometown;
 };
 
-let joe = (name, age, hometown) => ({name: name, age: age, hometown: hometown});
-//let joe = new Student('Joe', 'Schmoe', 100);
+let joe = new Student('Joe', 'Schmoe', 100);
 
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
 console.log(joe);
-console.log(newStudent('Joe', 'Schmoe', 100));
 // TODO: After viewing the previous console.log(), return the code to a working state.
-
-
 
 Student.prototype.greeting = function() {
   return `Hi, my name is ${this.name}`;
 };
 
 
-
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
@@ -191,7 +186,7 @@ Student.courseName = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
@@ -202,17 +197,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// This is the joe student object
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// It calls a browser window object.
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// An arrow function does not redefine this. So 'this.' retains the value as defined within the scope of where the arrow function was defined.
